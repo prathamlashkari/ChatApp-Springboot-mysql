4c06cfd9930de049a349e22c3a8c067a80fb5582
Pratham <prathamlashkari841@gmail.com>
2024-05-12 19:28:37 +0530

    Adding cors and csrf configuration

--- chat-app-Backend/src/main/java/com/pratham/chatappbackend/config/AppConfig.java
+++ chat-app-Backend/src/main/java/com/pratham/chatappbackend/config/AppConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -48,5 +50,12 @@ public class AppConfig {
         .and().formLogin().and().httpBasic(null);
 
     return http.build();
+
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
