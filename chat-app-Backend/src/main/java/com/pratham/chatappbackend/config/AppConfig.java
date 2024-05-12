4bb76f842d49f40a52b3036c3350ad6788183f52
Pratham <prathamlashkari841@gmail.com>
2024-05-12 19:25:06 +0530

    Adding cors and csrf configuration

--- chat-app-Backend/src/main/java/com/pratham/chatappbackend/config/AppConfig.java
+++ chat-app-Backend/src/main/java/com/pratham/chatappbackend/config/AppConfig.java
@@ -1,10 +1,19 @@
 package com.pratham.chatappbackend.config;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import java.util.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AppConfig {
@@ -13,7 +22,31 @@ public class AppConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**").authenticated()
+            .anyRequest().permitAll())
+        .addFilterBefore(null, null)
+        .csrf().disable()
+        .cors().configurationSource(new CorsConfigurationSource() {
+
+          @SuppressWarnings("null")
+          @Override
+          @Nullable
+
+          public CorsConfiguration getCorsConfiguration(HttpServletRequest arg0) {
+            CorsConfiguration cfg = new CorsConfiguration();
+            cfg.setAllowedOrigins(Arrays.asList("https://localhost:5173"));
+            cfg.setAllowedMethods(Collections.singletonList("*"));
+            cfg.setAllowCredentials(true);
+            cfg.setExposedHeaders(Arrays.asList("Authorization"));
+            cfg.setAllowedHeaders(Collections.singletonList("*"));
+            cfg.setMaxAge(3600L);
+            return cfg;
+          }
+
+        })
+        .and().formLogin().and().httpBasic(null);
+
     return http.build();
   }
 }
