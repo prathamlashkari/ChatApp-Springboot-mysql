635d6009342471b857c0db71c3f4357931df14fc
Pratham <prathamlashkari841@gmail.com>
2024-05-14 15:26:37 +0530

    Login done

--- chat-app-Backend/src/main/java/com/pratham/chatappbackend/Controller/AuthController.java
+++ chat-app-Backend/src/main/java/com/pratham/chatappbackend/Controller/AuthController.java
@@ -3,9 +3,12 @@ package com.pratham.chatappbackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,8 @@ import com.pratham.chatappbackend.exception.UserException;
 import com.pratham.chatappbackend.model.User;
 import com.pratham.chatappbackend.repository.UserRepository;
 import com.pratham.chatappbackend.response.AuthResponse;
+import com.pratham.chatappbackend.response.LoginRequest;
+import com.pratham.chatappbackend.service.CustomUserService;
 
 @RestController
 @RequestMapping("/auth")
@@ -26,6 +31,12 @@ public class AuthController {
 
   @Autowired
   private TokenProvider tokenProvider;
+
+  @Autowired
+  private CustomUserService customUserService;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
   @PostMapping("/signup")
   public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws UserException {
@@ -52,6 +63,34 @@ public class AuthController {
 
     AuthResponse res = new AuthResponse(jwt, true);
     return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+  }
+
+  public ResponseEntity<AuthResponse> loginHandler(@RequestBody LoginRequest req) {
+
+    String email = req.getEmail();
+    String password = req.getPassword();
+
+    Authentication authentication = authentication(email, password);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    String jwt = tokenProvider.generateToken(authentication);
+
+    AuthResponse res = new AuthResponse(jwt, true);
+    return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+
+  }
+
+  public Authentication authentication(String Username, String passwrod) {
+    UserDetails userDetails = customUserService.loadUserByUsername(Username);
+
+    if (userDetails == null) {
+      throw new BadCredentialsException("invalid username");
+    }
+    if (!passwordEncoder.matches(passwrod, userDetails.getPassword())) {
+      throw new BadCredentialsException("invalid password");
+    }
+
+    return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
   }
 
 }
---
+++ chat-app-Backend/src/main/java/com/pratham/chatappbackend/response/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.pratham.chatappbackend.response;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+  private String email;
+  private String password;
+
+  public LoginRequest(String email, String password) {
+    super();
+    this.email = email;
+    this.password = password;
+  }
+}
