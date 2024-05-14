a42de19e0141d49bafed5416bcb6427ad530e468
Pratham <prathamlashkari841@gmail.com>
2024-05-14 15:39:15 +0530

    Adding message entity

--- chat-app-Backend/src/main/java/com/pratham/chatappbackend/model/Message.java
+++ chat-app-Backend/src/main/java/com/pratham/chatappbackend/model/Message.java
@@ -1,5 +1,27 @@
 package com.pratham.chatappbackend.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Message {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer Id;
+  private String content;
+
+  private LocalDateTime timestamps;
+
+  @ManyToOne
+  private User user;
+
+  @ManyToOne
+  private Chat chat;
 
 }
