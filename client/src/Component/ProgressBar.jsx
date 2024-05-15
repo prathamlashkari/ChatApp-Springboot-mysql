56b37199aa0296985b51bb93f0fa52c65720e178
Pratham <prathamlashkari841@gmail.com>
2024-05-15 16:32:15 +0530

    Creating back func to staus bar

---
+++ client/src/Component/ProgressBar.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const ProgressBar = ({index,activeIndex,duration}) => {
+  const isActive = index === activeIndex;
+  const [progress , setProgress] = useState();
+
+  useEffect(()=>{
+    const intervalid = setInterval(() => {
+      setProgress((prev)=>{
+        if(prev<100)
+          return prev+1;
+
+        clearInterval(intervalid);
+        return prev;
+      })
+    }, duration/100);
+  },[duration,activeIndex]);
+
+  useEffect(()=>{
+    setProgress(0)
+  },[activeIndex])
+  return (
+    <div className={`progress-bar-container ${isActive?"active":""}`}>
+      <div 
+      className={`${isActive ? "progress-bar" : ""}`} 
+      style={{width:`${progress}%`}} >
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
--- client/src/pages/StateViewer.jsx
+++ client/src/pages/StateViewer.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {stories} from '../assets/constant'
+import ProgressBar from '../Component/ProgressBar';
+import { IoIosArrowBack } from 'react-icons/io';
 const StateViewer = () => {
   const [currentStoryIndex , setCurrentStoryIndex] = useState(0);
   const [activeIndex , setActiveIndex] = useState(0);
@@ -28,6 +30,18 @@ const StateViewer = () => {
           <img  
           className='max-h-[96vh] object-contain' 
           src={stories?.[currentStoryIndex].image} alt="" />
+          <div className='absolute top-0 flex w-full'>
+              {
+                stories.map((item , ind) => <ProgressBar 
+                key={ind} 
+                duration={2000} 
+                index={ind} 
+                activeIndex={activeIndex}/> )
+              }
+          </div>
+        </div>
+        <div>
+          <IoIosArrowBack/>
         </div>
       </div>
     </div>
