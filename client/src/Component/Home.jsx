d1379711c51fb22bdda68248191b1054d7401abd
Pratham <prathamlashkari841@gmail.com>
2024-05-15 10:29:32 +0530

    Home page created ui

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { BiCommentDetail } from 'react-icons/bi'
-import { BsFilter } from 'react-icons/bs'
+import { BsEmojiSmile, BsFilter, BsMicFill, BsThreeDotsVertical } from 'react-icons/bs'
 import { TbCircleDashed } from 'react-icons/tb'
 import dashimg from '../assets/dashimg.jpg'
 import ChatCard from './ChatCard'
+import MessageCard from './MessageCard'
+import { ImAttachment } from 'react-icons/im'
+import "../index.css"
 const Home = () => {
 
-  const [query,setQuery] = useState("");
+  const [query,setQuery] = useState("dd");
+  const [content,setContent] = useState("");
   const [currentChat , setCurrentChat] = useState(false);
 
+  const handleChat =()=>{
+    setCurrentChat(true);
+  }
   
   const hadnleSearch =()=>{
   }
+  const handleCreateMessage =()=>{}
+
   return (
     <div className="relative">
-      <div className="py-14 bg-[#00a884] w-full">
-        <div className="flex bg-[#f0f2f5] h-[90vh] absolute top-6 left-6 w-full">
+      <div className="py-14 bg-[#00a884] w-full"></div>
+        <div className="flex bg-[#f0f2f5] h-[90vh] absolute top-[5vh] left-[2vw] w-[90vw]">
           
             <div className="left w-[30%] bg-[#e8e9ec] h-full">
               <div className="w-full">
@@ -39,19 +48,19 @@ const Home = () => {
                   hadnleSearch(e.target.value)
                 }}
                 value={query} />
-                <AiOutlineSearch className="left-9 top-7 absolute"/>
+                <AiOutlineSearch className="left-5 top-6 absolute"/>
                 <div>
                   <BsFilter className="ml-4 text-3xl"/>
                 </div>
               </div>
               {/* All users */}
               <div className="bg-white overflow-y-scroll h-[76.8vh] px-4">
-              { [1,2,3,4,5].map((i)=> 
+              { query && [1,2,3,4,5].map((i)=> 
               <div 
-              key={i}
-              className="cursor-pointer"
-              onClick={() => setCurrentChat((prev) => !prev)}> 
-                <br/> 
+              key={i} 
+              className="cursor-pointer mb-2"
+              onClick={() =>handleChat()}> 
+               <hr /> 
               <ChatCard />
               </div>)}
               </div>
@@ -64,9 +73,56 @@ const Home = () => {
                    <p className="my-9">Send and receive message</p>
                  </div>
               </div>}
+              {/* Message part */}
+              {
+                currentChat && <div className="w-[70%] relative">
+                    <div className="header absolute top-0 w-full bg-[#f0f2f5]">
+                      <div className="flex justify-between">
+                        <div className="py-3 flex space-x-4 items-center px-3"> 
+                           <img
+                           className="w-10 h-10 rounded-full" 
+                           src="https://png.pngtree.com/png-vector/20220707/ourmid/pngtree-chatbot-robot-concept-chat-bot-png-image_5632381.png" alt="" />
+                           <p>username</p>
+                        </div>
+                        <div className="py-3 flex space-x-4 items-center px-3">
+                          <AiOutlineSearch/>
+                          <BsThreeDotsVertical/>
+                        </div>
+                      </div>
+                    </div>
+                    {/* message sections */}
+                    <div className="px-10 h-[85vh] overflow-y-scroll bg-blue-200">
+                        <div className="space-y-1 flex flex-col justify-center  mt-20 py-2">
+                          {[1,2,3,4,5].map((item,ind)=><MessageCard isReqMessage={ind%2==0} content={"messsagess"} />)}
+                        </div>
+                    </div>
+                    {/* Footer part */}
+                    <div className="footer bg-[#f0f2f5] bottom-0 w-full py-3 text-2xl">
+
+                    <div className="flex justify-between item-center px-5 relative">
+               
+                            <BsEmojiSmile className="cursor-pointer"/>
+                            <ImAttachment/>
+                            <input type="text"
+                            className="py-2 outline-none border-none bg-white pl-4 rounded-md w-[85%]" 
+                            onChange={(e)=>setContent(e.target.value)}
+                            value={content}
+                            placeholder="Type Message"
+                            onKeyPress={(e)=>{
+                              if(e.key === "Enter")
+                                {
+                                  handleCreateMessage();
+                                  setContent("")
+                                }
+                            }} />
+                            <BsMicFill/>
+                    </div>
+                      
+                    </div>
+                </div>
+              }
         </div>
       </div>
-    </div>
   )
 }
 
