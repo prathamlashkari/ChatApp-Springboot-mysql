c2517477d67bd97dee3c2f7a2d9c180aad151a8e
Pratham <prathamlashkari841@gmail.com>
2024-05-16 17:52:40 +0530

    Created both signup and sigin form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
  import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
  // src/index.js
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 import Home from "./pages/Home";
 import Status from "./pages/Status";
 import StateViewer from "./pages/StateViewer";
 import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 function App() {
 
 
@@ -17,8 +22,10 @@ function App() {
           <Route path="/status" element={<Status/>}/>
           <Route path="/status/:usreId" element={<StateViewer/>}/>
           <Route path="/signin" element={<Signin/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
          </Routes>
        </Router>
+       <ToastContainer/>
     </>
   )
 }
