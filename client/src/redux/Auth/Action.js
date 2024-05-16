39dceeb45965baad43f1ff25f7926f5ac1c1a280
Pratham <prathamlashkari841@gmail.com>
2024-05-16 10:19:20 +0530

    Adding login and signup page and integrate api

--- client/src/redux/Auth/Action.js
+++ client/src/redux/Auth/Action.js
@@ -1,5 +1,5 @@
 import {SERVER} from '../../config/api'
-import {REGISTER} from './ActionType'
+import {LOGIN, REGISTER, REQ_USER, SEARCH_USER, UPDATE_USER} from './ActionType'
 
 
 export const register =(data)=>async(dispatch)=>{
@@ -32,7 +32,62 @@ export const login =(data)=>async(dispatch)=>{
       })
       const resData = await res.json();
       console.log(`register ` , resData);
-      dispatch({type:REGISTER , payload:resData})
+      dispatch({type:LOGIN , payload:resData})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const currentUser =(token)=>async(dispatch)=>{
+  try {
+    const res = await fetch(`${SERVER}/auth/signin`,
+      {
+        method :"GET",
+        headers:{
+          "Content-type" : "application/json",
+           Authorization : `Bearer ${token}`
+        }
+      })
+      const resData = await res.json();
+      console.log(`register ` , resData);
+      dispatch({type:REQ_USER , payload:resData})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const searchUser =(data)=>async(dispatch)=>{
+  try {
+    const res = await fetch(`${SERVER}/users/search?name=${data.keyword}`,
+      {
+        method :"GET",
+        headers:{
+          "Content-type" : "application/json",
+           Authorization : `Bearer ${data.token}`
+        }
+      })
+      const resData = await res.json();
+      console.log(`register ` , resData);
+      dispatch({type:SEARCH_USER , payload:resData})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export const updateUser =(token)=>async(dispatch)=>{
+  try {
+    const res = await fetch(`${SERVER}/users/update/${data.id}`,
+      {
+        method :"PUT",
+        headers:{
+          "Content-type" : "application/json",
+           Authorization : `Bearer ${token}`
+        }
+      })
+      const resData = await res.json();
+      console.log(`register ` , resData);
+      dispatch({type:UPDATE_USER , payload:resData})
   } catch (error) {
     console.log(error)
   }
